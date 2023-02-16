ba977252836d9e5757c445c5990e684fb0e93d75
SwetlLOG <sweetlog@mail.ru>
2023-02-16 08:28:36 +0300

    Задача 2

---
+++ Seminar_2_Z2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или
+//сообщает, что третьей цифры нет 645-.5, 78->третьей цифры нет, 32679->6
+
+//Console.Write("Введите заданное число ");
+//String value = Console.ReadLine();
+//int resalt = Convert.ToInt32 (value);
+
+int value = 645;
+
+if ( value <= 999)
+{
+
+  int resalt = value % 10;
+  Console.WriteLine(resalt);
+}
+
+
+
+if (value >= 999 )
+{
+  int resalt = value/100 % 10;
+
+  Console.WriteLine(resalt);
+}
+if (value < 100)
+
+{
+  Console.WriteLine("Третьей цифры нет");
+}
