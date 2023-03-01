fd7c1942ccc07f25eca141d617b5a5943c383a01
SwetlLOG <sweetlog@mail.ru>
2023-03-01 20:55:54 +0400

    Задача 3

---
+++ Seminar_4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]      6, 1, 33 -> [6, 1, 33]
+
+//using System;
+
+
+//class Program
+//{
+//static void Main(string[] args)
+
+
+
+int[] number = { 1, 2, 5, 7, 19 };
+Console.Write("[");
+for (int i = 0; i < number.Length; i++)
+
+{
+  Console.Write($"{ number[i]}");
+}
+
+Console.Write("]");
+
+
+//}
