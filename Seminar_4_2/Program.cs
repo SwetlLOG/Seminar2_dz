af9e5818927a3cd2e05f6483a24dcdea0f8a5ca6
SwetlLOG <sweetlog@mail.ru>
2023-03-01 19:51:27 +0400

    Задача 2

---
+++ Seminar_4_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11 82 -> 10  9012 -> 12
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите целое число число   ");
+        var number = Convert.ToInt32(Console.ReadLine());
+        var sum = 0;
+        while(number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
+ 
+    
+    
+    
+   
+
+
+
+
+
+
+
