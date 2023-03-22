ee1153f48c4023bab9fe4664387d4b2dfabd74e5
SwetlLOG <sweetlog@mail.ru>
2023-03-22 14:53:12 +0400

    Добавили файл Programm_CS Итоговой работы

---
+++ Itogi_Profile_1SQ/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string[] Arr1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] Arr2 = new string[Arr1.Length];
+
+void ConditionCheck(string[] Arr1, string[] Arr2)
+{
+  int count = 0;
+  for (int i = 0; i < Arr1.Length; i++)
+  {
+    if(Arr1[i].Length <= 3)
+    { 
+      Arr2[count] = Arr1[i];
+      count++;
+    }
+  }
+}
+
+void PrintArray(string[] Array)
+{
+  for (int i = 0; i < Array.Length; i++)
+  {
+    Console.Write($"{Array[i]}");
+  }
+  Console.WriteLine();
+}
+
+ConditionCheck(Arr1, Arr2);
+PrintArray(Arr2);
